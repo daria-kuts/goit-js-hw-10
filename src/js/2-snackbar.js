df1b967609b8871389e9c508d91d98f33ff5ebe3
Daria <tkachenko.darya95@gmail.com>
2025-10-14 15:17:05 +0300

    create promise

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,44 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+const delayInput = form.querySelector('[name="delay"]');
+const stateInput = form.querySelector('[name="state"]');
+const delay = Number(delayInput.value);
+const state = stateInput.value;
+
+    if (isNaN(delay) || delay < 0) {
+        iziToast.error({
+  title: 'Error',
+  message: 'Illegal operation',
+  position: 'topRight',
+        });
+        return;
+    };
+    
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay)
+    });
+    promise
+        .then(value => {
+            iziToast.success({
+                title: 'OK',
+                message: `✅ Fulfilled promise in ${value}ms`,
+                position: 'topRight'
+            });
+        })
+        .catch(error => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${error}ms`,
+                position: 'topRight',
+            })
+        })
+})
